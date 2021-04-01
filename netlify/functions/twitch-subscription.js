3f85d86a05170583ca6c491958c6a06008ffc025
Jason Lengstorf <jason@lengstorf.com>
2021-04-01 10:30:14 -0700

    feat: show subscriptions!

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -2,12 +2,23 @@ exports.handler = async (event) => {
   const { headers = [] } = event;
 
   const type = headers['twitch-eventsub-message-type'] || 'no type';
+  const eventType = headers['twitch-eventsub-subscription-type'];
+
+  if (type !== 'notification' || eventType !== 'channel.subscribe') {
+    return { statusCode: 200, body: '' };
+  }
+
+  console.log({ headers: event.headers, body: event.body });
+
+  // at this point you know you have a valid subscription
+  // want to do some magic? put it here!
+  const { event: twitchEvent } = JSON.parse(event.body);
+  const user = twitchEvent.user_name;
+
+  console.log(`${user} just subscribed!`);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      type,
-      message: 'ok',
-    }),
+    body: '',
   };
 };

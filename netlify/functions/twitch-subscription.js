7503874092e41c757dce79d26177f01ab91ab83c
Jason Lengstorf <jason@lengstorf.com>
2021-04-01 09:57:46 -0700

    feat: pushing buttons to see if it works

---
+++ netlify/functions/twitch-subscription.js
@@ -0,0 +1,13 @@
+exports.handler = async (event) => {
+  const { headers = [] } = event;
+
+  const type = headers['twitch-eventsub-message-type'] ?? 'no type';
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      type,
+      message: 'ok',
+    }),
+  };
+};
